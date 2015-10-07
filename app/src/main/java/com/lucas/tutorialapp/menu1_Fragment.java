2b191924fcabe3f84282611614570a302778effa
LBRUNI <lucasbruni99@gmail.com>
2015-10-07 15:17:09 -0300

    Initial commit

---
+++ app/src/main/java/com/lucas/tutorialapp/menu1_Fragment.java
@@ -0,0 +1,21 @@
+package com.lucas.tutorialapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Lucas on 05/10/2015.
+ */
+public class menu1_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu1_layout,container,false);
+        return rootview;
+    }
+}
